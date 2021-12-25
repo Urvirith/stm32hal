c2e27d0c0c6e7123e325f3fa470c8e44054a53e4
Jake Mussler <jake.mussler@protonmail.com>
2021-12-25 15:16:29 +0000

    Updated library

--- mod.rs
+++ mod.rs
@@ -1,9 +1,10 @@
 /* Public Modules */
 pub mod common;
-pub mod rcc;
-pub mod gpio;
 pub mod timer;
 pub mod usart;
+pub mod gpio;
+pub mod nvic;
+pub mod rcc;
 pub mod i2c;
 pub mod spi;
 pub mod can;
--- nvic.rs
+++ nvic.rs
@@ -1,31 +1,14 @@
 
 // NVIC DRIVER
 // ARM NVIC MODULE
-// NVIC Description - is on pg 529
-// NVIC Registers (Programming Manual) - is on 178
+// Found in the board specific 
 
 /* Nested vectored interrupt controller (NVIC) */
 
+use crate::board::nvic::NVICReg;
 use super::pointer;
 
 const U32SIZE:						u32 = 32;
-
-#[repr(C)]
-struct NVICReg {
-	pub iser:		[u32; 16],		/* Interrupt Set-Enable register */
-	pub reserved0:	[u32; 16],
-	pub icer:		[u32; 16], 		/* Interrupt Clear-Enable register */
-	pub reserved1:	[u32; 16],
-	pub ispr:		[u32; 16],    	/* Interrupt Set-Pending Registers */
-	pub reserved2:	[u32; 16],
-	pub icpr:		[u32; 16],	    /* Interrupt Clear-Pending Registers */
-	pub reserved3:	[u32; 16],
-	pub iabr:		[u32; 16], 		/* Interrupt Active Bit Registers */
-    pub reserved4:	[u32; 16],
-	pub itns:		[u32; 16],	    /* Interrupt Target Non-Secure Registers */
-	pub reserved5:	[u32; 16],
-	pub ipr:		[u8; 480],	    /* Interrupt Priority Registers */
-}
 
 pub struct Nvic {
 	registers: &'static mut NVICReg
